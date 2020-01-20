233e8122f1de4d7de297397ff77ff8de05e29c17
ry-blank <ry_blank@hotmail.com>
2020-01-19 19:11:16 -0600

    test git assignment

---
+++ wdv341/testGit.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Test Git</title>
+    </head>
+
+    <body>
+        <?php
+            $firstName = "Ryan";
+        ?>
+
+
+        <h1>Test Git</h1>
+
+        <h2>The line below was written in PHP</h2>
+
+        <?php
+            echo "<h3>Hello, my name is " . $firstName . "!";
+        ?> 
+
+    </body>
+
+</html>
