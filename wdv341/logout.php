12fba2b74d56df2c7822791e84003b3187f1cb1b
ry-blank <52143833+ry-blank@users.noreply.github.com>
2020-05-04 13:27:56 -0500

    Create a Logout Page
     UNIT-11 Login and Protected Pages: Assignment: Create a logout.php page

---
+++ wdv341/logout.php
@@ -0,0 +1,13 @@
+<?php
+session_cache_limiter('none');			//This prevents a Chrome error when using the back button to return to this page.
+session_start();	//provide access to the current session
+
+
+$_SESSION['validUser'] = false;
+session_unset();	//remove all session variables related to current session
+session_destroy();	//remove current session
+
+header('Location: login.php');
+
+
+?>
